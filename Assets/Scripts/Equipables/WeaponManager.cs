e7eb7889874f2a0a93148f7efa2878e6a6121393
RockMuncher7 <106937171+RockMuncher7@users.noreply.github.com>
2023-07-07 15:46:05 +1000

    Fixed lighting shit and added some comments for Tom

--- Assets/Scripts/Equipables/WeaponManager.cs
+++ Assets/Scripts/Equipables/WeaponManager.cs
@@ -14,6 +14,21 @@ public class WeaponManager : MonoBehaviour
 
     [SerializeField] TextMeshProUGUI playerArrowsLeftText;
 
+    //TODO: Make a List that holds all weapon objects
+    //TODO: Make an integer that holds the weapon select ID (E.G. if you press 2, you will select weapon 2 from the list)
+    //TODO: Enable and disable weapons based on your keypress (E.G. You select weapon 2, it enables the bow, and disables the sword and bombs)
+
+    /*
+            for(int i = 0; i < weaponSelect[].Length; i++)
+            {
+                if (weaponSelect[id] == weaponSelect[i]) continue;
+
+                weaponSelect[i].SetActive(false);
+            }
+
+    JUST AN EXAMPLE
+    */
+
     private void OnEnable()
     {
         // Draw Bow Enable
@@ -51,6 +66,7 @@ public class WeaponManager : MonoBehaviour
         {
             bowObject.SetActive(true);
             swordObject.SetActive(false);
+
         }
         else
         {
