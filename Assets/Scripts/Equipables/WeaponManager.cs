68728b5040cd7078575a32655c7afef06fff0428
Thomas Graham <thomasgraham874@gmail.com>
2023-07-12 02:20:56 +1000

    weapon switch works but VERY buggy. Commented out for now

--- Assets/Scripts/Equipables/WeaponManager.cs
+++ Assets/Scripts/Equipables/WeaponManager.cs
@@ -8,6 +8,8 @@ public class WeaponManager : MonoBehaviour
 {
     public GameObject bowObject;
     public GameObject swordObject;
+    public List<GameObject> weaponList;
+    int selectedWeapon = 0;
     [SerializeField] PlayerInput playerInput;
 
     BowController _bowController;
@@ -18,16 +20,7 @@ public class WeaponManager : MonoBehaviour
     //TODO: Make an integer that holds the weapon select ID (E.G. if you press 2, you will select weapon 2 from the list)
     //TODO: Enable and disable weapons based on your keypress (E.G. You select weapon 2, it enables the bow, and disables the sword and bombs)
 
-    /*
-            for(int i = 0; i < weaponSelect[].Length; i++)
-            {
-                if (weaponSelect[id] == weaponSelect[i]) continue;
 
-                weaponSelect[i].SetActive(false);
-            }
-
-    JUST AN EXAMPLE
-    */
 
     private void OnEnable()
     {
@@ -57,6 +50,18 @@ public class WeaponManager : MonoBehaviour
     void Update()
     {
         playerArrowsLeftText.text = "Arrows left: " + _bowController.arrowsLeft;
+
+        //if (Input.GetKeyDown(KeyCode.Q))
+        //{
+        //    selectedWeapon = 0;
+        //    WeaponSwitch();
+        //}
+        //if (Input.GetKeyDown(KeyCode.E))
+        //{
+        //    selectedWeapon = 1;
+        //    WeaponSwitch();
+        //}
+
     }
 
     void WeaponSwitch(InputAction.CallbackContext context)
@@ -73,10 +78,26 @@ public class WeaponManager : MonoBehaviour
             bowObject.SetActive(false);
             swordObject.SetActive(true);
         }
-    }
 
-    void ResetSwordPosition()
-    {
-
+        // THIS WORKS. THE SWORD HOWEVER CANNOT BE SWUNG AFTERWARDS AND BOW SHOOTS TWICE. UNKNOWN REASON. WILL LOOK INTO IT AGAIN
+        //for (int i = 0; i < weaponList.Count; i++)
+        //{
+        //    if (weaponList[selectedWeapon] != weaponList[i])
+        //    {
+        //        weaponList[i].SetActive(false);
+        //    }
+        //    weaponList[i].gameObject.SetActive(i == selectedWeapon);
+        //}
     }
 }
+
+/*
+        for(int i = 0; i < weaponSelect[].Length; i++)
+        {
+            if (weaponSelect[id] == weaponSelect[i]) continue;
+
+            weaponSelect[i].SetActive(false);
+        }
+
+JUST AN EXAMPLE
+*/
