45a5ed3e297fb36260887c9a166eb1b65570f57c
moomooushi <lily.fields1999@gmail.com>
2023-07-14 17:17:07 +1000

    Added more torches in world.

--- Assets/Scripts/PuzzleRelated/PlayerInteract.cs
+++ Assets/Scripts/PuzzleRelated/PlayerInteract.cs
@@ -36,12 +36,6 @@ public class PlayerInteract : MonoBehaviour
         interactKeyPressed.Disable();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
