fb9b168782e26fdf5793e07e9f4c3d705c50bbe7
moomooushi <lily.fields1999@gmail.com>
2023-07-14 16:48:10 +1000

    Interact script and activatable torch done.

---
+++ Assets/Scripts/PuzzleRelated/PlayerInteract.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+
+public class PlayerInteract : MonoBehaviour
+{
+    bool playerInRange;
+
+    [SerializeField] PlayerInput PlayerInput;
+
+    [SerializeField] UnityEvent onInteractEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerInput = GetComponent<PlayerInput>();
+    }
+
+    private void OnEnable()
+    {
+        InputAction interactKeyPressed = PlayerInput.actions["Interact"];
+
+        interactKeyPressed.started += OnInteract;
+
+        interactKeyPressed.Enable();
+    }
+
+    private void OnDisable()
+    {
+        InputAction interactKeyPressed = PlayerInput.actions["Interact"];
+
+        interactKeyPressed.started -= OnInteract;
+
+        interactKeyPressed.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            playerInRange = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        playerInRange = false;
+    }
+
+    void OnInteract(InputAction.CallbackContext context)
+    {
+        if (playerInRange)
+        {
+            onInteractEvent.Invoke();
+        }
+    }
+}
