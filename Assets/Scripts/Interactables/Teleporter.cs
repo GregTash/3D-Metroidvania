d6c14546ab9be390d7101d981d0000fb4d642f85
RockMuncher7 <Gregtash4@gmail.com>
2023-11-22 14:29:09 +1100

    Weird glow cave water glitch fixed

--- Assets/Scripts/Interactables/Teleporter.cs
+++ Assets/Scripts/Interactables/Teleporter.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Teleporter : MonoBehaviour
 {
-    [SerializeField] Transform teleportTransform;
-
     private void OnTriggerEnter(Collider collision)
     {
-        Teleport(collision.transform);
-    }
-
-    void Teleport(Transform teleportObj)
-    {
-        teleportObj.transform.position = teleportTransform.position;
+        collision.TryGetComponent(out PlayerManager playerManager);
+        {
+            playerManager.TakeDamage(10000);
+        }
     }
 }
