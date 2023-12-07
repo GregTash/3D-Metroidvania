42fe37180b527afd411222978a4d3179e3ce033b
Thomas Graham <thomasgraham874@gmail.com>
2023-12-07 22:18:53 +1100

    Working on Teleport script

--- Assets/Scripts/Interactables/Teleporter.cs
+++ Assets/Scripts/Interactables/Teleporter.cs
@@ -1,14 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Teleporter : MonoBehaviour
 {
+    [SerializeField] private Transform teleportLocation;
+    [SerializeField] private float timeUntilNextTeleport;
+    [SerializeField] private float timer;
+    [SerializeField] private PlayerManager playerManger;
+
+    private void Update()
+    {
+        if (!playerManger.canTeleport)
+        {
+            timer += Time.deltaTime;
+            if (timer >= timeUntilNextTeleport)
+            {
+                playerManger.canTeleport = true;
+            }
+        }
+    }
+
     private void OnTriggerEnter(Collider collision)
     {
-        // collision.TryGetComponent(out PlayerManager playerManager);
-        // {
-        //     playerManager.TakeDamage(10000);
-        // }
+        collision.TryGetComponent(out PlayerManager playerManager);
+        {
+            if (playerManager && playerManager.canTeleport)
+            {
+                collision.transform.position = teleportLocation.position;
+                playerManager.canTeleport = false;
+            }
+        }
     }
 }
