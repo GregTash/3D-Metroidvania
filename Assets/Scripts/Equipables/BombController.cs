d40d221a4d6a0e190b7efa514c6c96b1fa480290
RockMuncher7 <Gregtash4@gmail.com>
2023-12-10 19:44:56 +1100

    Bounce pad sound

--- Assets/Scripts/Equipables/BombController.cs
+++ Assets/Scripts/Equipables/BombController.cs
@@ -9,6 +9,8 @@ public class BombController : MonoBehaviour
     PlayerControls _playerControls;
 
     [SerializeField] GameObject bombObj;
+    [SerializeField] float timeBeforeThrowAgain = 1.0f;
+    float _tempTimeBeforeThrowAgain = 0.0f;
 
     private void Awake()
     {
@@ -19,20 +21,34 @@ public class BombController : MonoBehaviour
     private void OnEnable()
     {
         playerInput.actions["Attack"].started += OnThrow;
+        _tempTimeBeforeThrowAgain = 0.0f;
     }
 
     private void OnDisable()
     {
         playerInput.actions["Attack"].started -= OnThrow;
+        _tempTimeBeforeThrowAgain = 0.0f;
+    }
+
+    private void Update()
+    {
+        if (_tempTimeBeforeThrowAgain > 0)
+        {
+            _tempTimeBeforeThrowAgain -= Time.deltaTime;
+        }
     }
 
     void OnThrow(InputAction.CallbackContext context)
     {
         if (playerManager.bombs > 0)
         {
+            if (_tempTimeBeforeThrowAgain > 0) return;
+
             GameObject newBomb = Instantiate(bombObj, transform.position + (transform.forward) + new Vector3(0, 1, 0), bombObj.transform.rotation);
             newBomb.GetComponent<Rigidbody>().AddForce(playerCameraTransform.forward * 50, ForceMode.Impulse);
             playerManager.bombs--;
+
+            _tempTimeBeforeThrowAgain = timeBeforeThrowAgain;
         }
     }
 }
--- Assets/Scripts/Interactables/JumpPad.cs
+++ Assets/Scripts/Interactables/JumpPad.cs
@@ -3,6 +3,12 @@ using UnityEngine;
 public class JumpPad : MonoBehaviour
 {
     [SerializeField] float jumpForce = 15f;
+    AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -14,6 +20,8 @@ public class JumpPad : MonoBehaviour
             if (animator != null) animator.Play("Jump");
             rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
             rb.AddForce(jumpForce * transform.up, ForceMode.Impulse);
+
+            _audioSource.Play();
         }
     }
 }
