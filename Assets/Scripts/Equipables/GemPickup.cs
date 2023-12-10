5bebe3d6567b790bf65108fe0fa20f5805c53cb5
RockMuncher7 <Gregtash4@gmail.com>
2023-12-10 20:20:44 +1100

    Worked on gems

--- Assets/Scripts/Equipables/GemPickup.cs
+++ Assets/Scripts/Equipables/GemPickup.cs
@@ -3,8 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class GemPickup : MonoBehaviour
 {
+    AudioSource _audioSource;
+    Animator _animator;
+
     private void Start()
     {
+        _audioSource = GetComponent<AudioSource>();
+        _animator = GetComponent<Animator>();
+
         if (PlayerPrefs.GetInt(transform.name) > 0)
         {
             Destroy(gameObject);
@@ -17,10 +23,19 @@ public class GemPickup : MonoBehaviour
 
         if (!player) return;
 
+        _audioSource.Play();
+
         player.gemsCollected++;
 
         PlayerPrefs.SetInt(transform.name, 0);
 
+        GetComponent<Collider>().enabled = false;
+
+        _animator.Play("Collect");
+    }
+
+    public void DestroyGem()
+    {
         Destroy(gameObject);
     }
 }
